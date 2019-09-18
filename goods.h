f85873d9146871e89300b677448874d505ba1198
Jonghyun Seo <jonghyun0101.seo@gmail.com>
2019-09-18 15:32:27 +0900

    Update goods.h
    변경되지 않는 SetGoodsInfo 함수 파라미터 const 지정

--- goods.h
+++ goods.h
@@ -8,7 +8,7 @@
 //    std::cout << test.GetIndex() << " - " << test.GetPrice << endl;
 class Goods{
  public:
-  void SetGoodsInfo(int index, int price);
+  void SetGoodsInfo(const int index, const int price);
   int GetIndex();
   int GetPrice();
 
